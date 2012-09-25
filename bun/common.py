ef3c7265f1f3177024867b2b32c75fe8f8400314
Bruno Azzinnari <bazzinnari@gmail.com>
2012-09-25 13:41:09 -0300

    Class for SentenceProvider to replace SentenceGrabber.

---
+++ bun/common.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class Singleton(type):
+    """
+    Metaclass to implement Singleton Pattern.
+
+    """
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+
+    def __call__(cls,*args,**kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
+
+
--- bun/kanjidic.py
+++ bun/kanjidic.py
@@ -4,6 +4,7 @@ import sys
 import string
 
 from lxml import etree
+from common import Singleton
 
 
 class Kanji(object):
@@ -22,19 +23,6 @@ class Kanji(object):
 
     def __unicode__(self):
         return u"<Kanji: %s (u:%d,k:%d)>" % (self.literal, self.idx_unicode, self.idx_kolivas)
-
-
-# metaclass to implement Singleton Pattern
-class Singleton(type):
-    def __init__(cls, name, bases, dict):
-        super(Singleton, cls).__init__(name, bases, dict)
-        cls.instance = None
-
-    def __call__(cls,*args,**kw):
-        if cls.instance is None:
-            cls.instance = super(Singleton, cls).__call__(*args, **kw)
-        return cls.instance
-
 
 
 class KanjiDic(object):
